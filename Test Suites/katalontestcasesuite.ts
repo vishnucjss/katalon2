dd45d585f722ffcd5d2b52f8d5670b4544cc6e1f
vishnu <k.v.v.reddy98@gmail.com>
2025-01-15 19:42:47 +0800

    second commit

---
+++ Test Suites/katalontestcasesuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>katalontestcasesuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9ecf9476-10fb-4a7d-8678-807f65c42032</testSuiteGuid>
+   <testCaseLink>
+      <guid>939c8615-c660-4516-bc71-c071c17af193</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/katalontestcase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
